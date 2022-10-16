83eed594da04f03975a8e245e88ef5d4aa1245ce
Sudhansu-Sekhar-Padhy <sidsekhar809@gmail.com>
2022-10-16 23:12:49 +0530

    1Array.js

---
+++ Create an array.js
@@ -0,0 +1,19 @@
+//create an array - array literal[]
+let array1 = ["Bhagwat","Gita"]
+console.log(array1)//[ 'Bhagwat','Gita']
+
+
+
+let MyList = [];
+console.log(MyList)//[]empty array
+
+let numberArray =[2,4,6,8]
+console.log(numberArray);//[ 2,4,6,8 ]-array numbers
+
+let StringArray =['eat','work','sleep'];
+console.log(StringArray);//[ 'eat', 'work', 'sleep' ] -array string
+
+let newData =['work','exercise',1,true]
+console.log(newData);// string , number , boolean 
+
+
